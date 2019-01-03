5357c66c1c0b4c6d19f3d8d1fb921d0ebfeedb84
Matthew Huie <matthew@matthewhuie.com>
2019-01-03 15:55:28 -0500

    Fixing async method for 'authenticate'

--- lib/foursquare_oauth.dart
+++ lib/foursquare_oauth.dart
@@ -6,8 +6,8 @@ class FoursquareOAuth {
   static const MethodChannel _channel =
       const MethodChannel('com.matthewhuie.flutter.foursquare.oauth');
 
-  static Future<String> authenticate(String clientId, String clientSecret)  {
-    return _channel.invokeMethod('authenticate', <String, String> {
+  static Future<String> authenticate(String clientId, String clientSecret) async {
+    return await _channel.invokeMethod('authenticate', <String, String> {
       'clientId': clientId,
       'clientSecret': clientSecret
     });
